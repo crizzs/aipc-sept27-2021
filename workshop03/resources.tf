467ff1e9972738097daa1f67b18b01f8e0b04589
root <crizzs_2@hotmail.com>
2021-09-29 07:01:44 +0000

    Add tf script to provision image

---
+++ workshop03/resources.tf
@@ -0,0 +1,39 @@
+data "digitalocean_droplet_snapshot" "mysql8_snapshot" {
+  name_regex  = "^mysql8"
+  region      = "sgp1"
+  most_recent = true
+}
+
+data digitalocean_ssh_key aipc {
+  name = "test-machine"
+}
+
+variable DO_token {
+  type = string
+  sensitive = true
+}
+
+variable DO_region {
+  type = string
+  default = "sgp1"
+}
+
+variable DO_size {
+  type = string
+  default = "s-1vcpu-1gb"
+}
+
+
+resource "digitalocean_droplet" "mysql8_droplet" {
+  name = "mysql8-droplet"
+  region = var.DO_region
+  size = var.DO_size
+  image  = data.digitalocean_droplet_snapshot.mysql8_snapshot.id
+  ssh_keys = [data.digitalocean_ssh_key.aipc.id]
+
+}
+
+output droplet_ipv4 {
+    value = digitalocean_droplet.mysql8_droplet.ipv4_address
+}
+  
